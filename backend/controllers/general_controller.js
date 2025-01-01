dfa81c7a6a1f0e282bc7ca3f64014c0393a4a66d
Rakshit-Oswal <rudyons2005@gmail.com>
2025-01-01 19:09:04 +0530

    New commit

--- backend/controllers/general_controller.js
+++ backend/controllers/general_controller.js
@@ -2,8 +2,25 @@ const Resource = require('../models/resource');
 
 exports.getResources = async (req, res) => {
     try {
-        const resources = await Resource.find();
-        res.status(200).json(resources);
+        const page = parseInt(req.query.page, 10) || 1; // Default to page 1
+        const limit = parseInt(req.query.limit, 10) || 20; // Default to 20 entries per page
+        const startIndex = (page - 1) * limit;
+
+        console.log(`Page: ${page}, Limit: ${limit}, StartIndex: ${startIndex}`);
+
+        const resources = await Resource.find().skip(startIndex).limit(limit);
+        console.log(resources);
+
+        const totalCount = await Resource.countDocuments();
+
+        res.status(200).json({
+            metadata: {
+                totalCount,
+                currentPage: page,
+                totalPages: Math.ceil(totalCount / limit),
+            },
+            data: resources,
+        });
     } catch (error) {
         res.status(500).json({ error: 'Error fetching resources' });
     }
--- backend/models/resource.js
+++ backend/models/resource.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 
 const resourceSchema = new mongoose.Schema({
     hospitalName: { type: String, required: true },
+    cityName: { type: String, required: true },
+    zoneName: { type: String, required: true },
+    wardName: { type: String, required: true },
     totalBeds: { type: Number, required: true },
     availableBeds: { type: Number, required: true },
-    equipment: [
-        {
-            name: { type: String },
-            quantity: { type: Number },
-        },
-    ],
+    facilityType: { type: String },
+    classType: { type: String },
+    level: { type: String },
+    pharmacyAvailable: { type: String },
+    ambulanceAvailable: { type: String },
+    countOfAmbulance: { type: Number },
+    averageMonthlyFootfall: { type: Number },
 });
 
 const Resource = mongoose.model('Resource', resourceSchema);
